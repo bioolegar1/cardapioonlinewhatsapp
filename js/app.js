9f94fdf4aa8348504c681827ee75055447c4cee4
bioolegar1 <bioolegari@gmail.com>
2024-09-21 14:29:32 -0300

    contador funcionando

--- js/app.js
+++ js/app.js
@@ -1,80 +1,74 @@
 $(document).ready(function () {
     cardapio.eventos.init();
-})
+});
 
-var cardapio ={};
+var cardapio = {};
 
 cardapio.eventos = {
-    init:() => {
+    init: () => {
         cardapio.metodos.obterItensCardapio();
-    }
+    },
+};
 
-}
-
-cardapio.metodos ={
+cardapio.metodos = {
     //obtem a lista de itens do cardápio. ----------------///////////////////////
-    obterItensCardapio: (categoria = 'burgers', vermais =false) => {
+    obterItensCardapio: (categoria = "burgers", vermais = false) => {
         var filtro = MENU[categoria];
-        console.log(filtro);
+        // console.log(categoria);
 
-        if(!vermais){
-            $("#itensCardapio").html('');
-            $("#btnVerMais").removeClass('hidden')
-
+        if (!vermais) {
+            $("#itensCardapio").html("");
+            $("#btnVerMais").removeClass("hidden");
         }
 
-        $.each(filtro, (i,e)=> {
-
+        $.each(filtro, (i, e) => {
             let temp = cardapio.templates.item
-                .replace(/\${img}/g, e.img )
+                .replace(/\${img}/g, e.img)
                 .replace(/\${name}/g, e.name)
-                .replace(/\${price}/g, e.price.toFixed(2) .replace('.',',')
-                .replace(/\${id}/g, e.id ))
-            //botão ver mais foi clicado (12 itens)
-            if(vermais && i >= 8 && i < 12) {
-                $("#itensCardapio").append(temp)
-            }
-            //paginação inicial 8 itens
-            if (!vermais && i < 8) {
-                $("#itensCardapio").append(temp)
+                .replace(/\${price}/g, e.price.toFixed(2).replace(".", ","))
+                .replace(/\${id}/g, e.id);
 
+            // botão ver mais foi clicado (12 itens)
+            if (vermais && i >= 8 && i < 12) {
+                $("#itensCardapio").append(temp);
             }
-        })
+            // paginação inicial 8 itens
+            if (!vermais && i < 8) {
+                $("#itensCardapio").append(temp);
+            }
+        });
+
         //remove o ativo
-        $(".container-menu a").removeClass('active');
+        $(".container-menu a").removeClass("active");
 
         //seta o menu para ativo
-        $("#menu-" + categoria).addClass('active');
+        $("#menu-" + categoria).addClass("active");
     },
 
     //Clique no botão de ver mais
     verMais: () => {
-        var ativo = $(".container-menu a.active").attr('id').split('menu-')[1]; //quebra o nameclass para facilitar o vermais para cada categoria
-        cardapio.metodos.obterItensCardapio(ativo,true)
-        $("#btnVerMais").addClass('hidden') //esconde o botão ver mais
+        var ativo = $(".container-menu a.active").attr("id").split("menu-")[1]; //quebra o nameclass para facilitar o vermais para cada categoria
+        cardapio.metodos.obterItensCardapio(ativo, true);
+        $("#btnVerMais").addClass("hidden"); //esconde o botão ver mais
     },
-
 
     //diminuir a quantidade do item no cardapio
     diminuirQuantidade: (id) => {
-        let qntdAtual = parseInt($("qntd-" + id).text());
+        let qntdAtual = parseInt($("#qntd-" + id).text());
 
-        if(qntdAtual > 0) {
-            $("qntd-" + id).text(qntdAtual - 1)
+        if (qntdAtual > 0) {
+            $("#qntd-" + id).text(qntdAtual - 1);
         }
-
     },
 
     //Aumentar a quantidade do item no cardapio
     aumentarQuantidade: (id) => {
-        let qntdAtual = parseInt($("qntd-" + id).text());
-        $("qntd-" + id).text(qntdAtual - 1)
-    }
+        let qntdAtual = parseInt($("#qntd-" + id).text());
+        $("#qntd-" + id).text(qntdAtual + 1);
+    },
+};
 
-}
-
-cardapio.templates ={
-
+cardapio.templates = {
     item: `   
     
             <div class="col-3 mb-5">
@@ -98,6 +92,5 @@ cardapio.templates ={
             </div>
     
         </div>
-`
-
-}
+`,
+};
